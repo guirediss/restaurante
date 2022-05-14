4abceaa2365b3b86f9a43b536358df4e4a641a2c
Guilherme R <guilherme.rediss@stormtech.com.br>
2022-05-14 18:56:28 -0300

    nem sei

--- src/app/informacoes-modal/informacoes-modal.component.ts
+++ src/app/informacoes-modal/informacoes-modal.component.ts
@@ -9,12 +9,16 @@ import { LanchesPage } from '../lanches/lanches.page';
 })
 export class InformacoesModalComponent implements OnInit {
 
+  lanches = new LanchesPage(this.modalController);
+  public id = this.lanches.getId();
+  public nomeLanche = this.lanches.lancheClicado(this.id);
 
-  lanches = LanchesPage;
-  
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController) {
+    console.log(this.nomeLanche);
+   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   fecharModal() {
     this.modalController.dismiss();
--- src/app/lanches/lanches.page.ts
+++ src/app/lanches/lanches.page.ts
@@ -9,7 +9,9 @@ import { InformacoesModalComponent } from '../informacoes-modal/informacoes-moda
 })
 export class LanchesPage implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController) {
+
+   }
 
   ngOnInit() {
   }
@@ -21,35 +23,61 @@ export class LanchesPage implements OnInit {
 
     await modal.present();
   }
-
-   public lanches = [
-    {
-      nome: 'X-Salada',
-      valor: 20.00,
-      ingredientes: 'Pão, Alface, Hambúrguer, Tomate, Maionese, Ketchup, Mostarda, Queijo'
-    },
-    {
-      nome: 'Cachorro Quente',
-      valor: 15.00,
-      ingredientes: 'Pão, Salsicha, Cebola, Tomate, Milho, Ervilha, Batata Palha'
-    },
-    {
-      nome: 'Pizza',
-      valor: 35.00,
-      ingredientes: '',
-      sabores: '4Queijos, Calabresa, Frango'
-    },
-    {
-      nome: 'Calzone',
-      valor: 10.00,
-      ingredientes: '',
-      sabores: '4Queijos, Calabresa, Frango'
-    },
-    {
-      nome: 'Pastel',
-      valor: 10.00,
-      ingredientes: '',
-      sabores: '4Queijos, Calabresa, Frango'
-    }
+  public lanches = [
+   {
+     nome: 'X-Salada',
+     valor: 20.00,
+     ingredientes: 'Pão, Alface, Hambúrguer, Tomate, Maionese, Ketchup, Mostarda, Queijo'
+   },
+   {
+     nome: 'Cachorro Quente',
+     valor: 15.00,
+     ingredientes: 'Pão, Salsicha, Cebola, Tomate, Milho, Ervilha, Batata Palha'
+   },
+   {
+     nome: 'Pizza',
+     valor: 35.00,
+     ingredientes: '',
+     sabores: '4Queijos, Calabresa, Frango'
+   },
+   {
+     nome: 'Calzone',
+     valor: 10.00,
+     ingredientes: '',
+     sabores: '4Queijos, Calabresa, Frango'
+   },
+   {
+     nome: 'Pastel',
+     valor: 10.00,
+     ingredientes: '',
+     sabores: '4Queijos, Calabresa, Frango'
+   }
   ];
+  
+  public getId() {
+    let id = (<HTMLSelectElement>document.getElementById('lanche')).value;
+    
+    if(parseInt(id) == 1){
+      return this.lancheClicado(this.lanches[0]);
+    } 
+    if (parseInt(id) == 2){
+      return this.lancheClicado(this.lanches[1]);
+    }
+    else if (parseInt(id) == 3){
+      return this.lancheClicado(this.lanches[2]);
+    }
+    else if (parseInt(id) == 4){
+      return this.lancheClicado(this.lanches[3]);
+    }
+    else if (parseInt(id) == 5){
+      return this.lancheClicado(this.lanches[4]);
+    }
+    
+    return id;
+  }
+
+  public lancheClicado(lanche?): Object {
+    return lanche;
+  }
+
 }
